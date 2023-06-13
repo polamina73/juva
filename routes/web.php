212d375d0942ec02d824ca9fe970cfcf6144493f
polamina73 <pola.mina73@gmail.com>
2023-06-13 05:10:37 +0300

    Fix issue in route

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ Auth::routes([
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/', function () {
+Route::get('empty', function () {
     return view('empty');
 })->name('empty');
