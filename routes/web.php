b944f662b545c6f14753015e7bc991245b07dc99
polamina73 <pola.mina73@gmail.com>
2023-06-13 04:50:51 +0300

    Auth Done And Layouts added and the assets

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Auth::routes([
+    'register' => false
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () {
+    return view('empty');
+})->name('empty');
